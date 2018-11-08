054283a9fc7e1706d76de41e99a88b1a2c596667
Tariq Dilrosun <42801356+TariqD@users.noreply.github.com>
2018-11-08 09:14:56 +0100

    L

---
+++ Dom.js
@@ -0,0 +1,9 @@
+
+
+
+
+
+document.getElementById("paginatitel").innerText ="Mijn geweldige pagina";
+document.getElementById("subkop").innerText ="Mijn geweldige subpagina";
+document.getElementById("Foto1").src ="img/schrift-aurora-met-spiraal-a4-120blz-gel-schriften-2960kt-gelinieerd.jpg";
+document.getElementById("Foto2").src = "img/open-boek.jpg";
